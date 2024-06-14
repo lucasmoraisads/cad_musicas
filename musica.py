e4e669574f3925e9505006a2a28d1028936837ff
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-06-14 06:39:46 -0300

    criando pagina de cadastro

--- musica.py
+++ musica.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template
 
+class Musica:
+    def __init__(self, nome, cantorGrupoBanda, genero):
+        self.nome = nome
+        self.cantorBanda = cantorGrupoBanda
+        self.genero = genero
+
 
 app = Flask(__name__)
 
 
 @app.route('/musicas')
 def listaMusicas():
+    
+    musica01 = Musica('vai passa', 'gaab', 'pop')
+    musica02 = Musica('Flashback', 'Mc paulinho', 'Funk')
+    musica03 = Musica('Bem melhor', 'Lagum', 'Pop')
+    
+    lista = [musica01, musica02, musica03]
+    
     return render_template('lista_musica.html',
-                           titulo = 'Musica') 
+                           titulo = 'Musica', 
+                           musicas = lista) 
 
 
-app.run()
+@app.route('/cadastra')
+def cadastraMusica():
+    return render_template('cadastra_musica.html')
+
+
+app.run(debug=True)
