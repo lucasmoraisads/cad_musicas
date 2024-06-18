3e10061ef4790541a2c86bfde4bb6d66a93dda01
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-06-18 06:24:15 -0300

    login

--- musica.py
+++ musica.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 class Musica:
     def __init__(self, nome, cantorGrupoBanda, genero):
@@ -6,18 +6,19 @@ class Musica:
         self.cantorBanda = cantorGrupoBanda
         self.genero = genero
 
+musica01 = Musica('vai passa', 'gaab', 'pop')
+musica02 = Musica('Flashback', 'Mc paulinho', 'Funk')
+musica03 = Musica('Bem melhor', 'Lagum', 'Pop')
+    
+lista = [musica01, musica02, musica03]
+
 
 app = Flask(__name__)
 
 
-@app.route('/musicas')
+@app.route('/')
 def listaMusicas():
     
-    musica01 = Musica('vai passa', 'gaab', 'pop')
-    musica02 = Musica('Flashback', 'Mc paulinho', 'Funk')
-    musica03 = Musica('Bem melhor', 'Lagum', 'Pop')
-    
-    lista = [musica01, musica02, musica03]
     
     return render_template('lista_musica.html',
                            titulo = 'Musica', 
@@ -29,4 +30,15 @@ def cadastraMusica():
     return render_template('cadastra_musica.html')
 
 
+@app.route('/adicionar', methods=['POST',])
+def adicionar_musica():
+    nome = request.form['nome']
+    cantor = request.form['cantor']
+    genero = request.form['genero']
+    
+    novaMusica = Musica(nome, cantor, genero)
+    
+    lista.append(novaMusica)
+
+    return redirect('/')
 app.run(debug=True)
